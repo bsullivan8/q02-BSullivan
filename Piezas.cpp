d40307985d95f28eca59ed3584b38f5edc8c0ede
Dr Kevin Buffardi <kbuffardi@csuchico.edu>
2017-10-09 05:24:46 +0000

    Functions implemented and test cases written

--- Piezas.cpp
+++ Piezas.cpp
@@ -1,5 +1,8 @@
 #include "Piezas.h"
 #include <vector>
+#include <iostream>
+
+using namespace std;
 /** CLASS Piezas
  * Class for representing a Piezas vertical board, which is roughly based
  * on the game "Connect Four" where pieces are placed in a column and 
@@ -22,6 +25,17 @@
 **/
 Piezas::Piezas()
 {
+	turn = X;
+	for(int i =0; i<BOARD_ROWS;i++)
+	{
+		std::vector<Piece> temp;	
+		for(int j=0; j<BOARD_COLS;j++)
+		{
+			temp.push_back(Blank);
+		}
+		board.push_back(temp);
+	}
+	
 }
 
 /**
@@ -30,6 +44,10 @@ Piezas::Piezas()
 **/
 void Piezas::reset()
 {
+	turn = X;
+	for(int i=0; i<BOARD_ROWS;i++)
+		for(int j=0;j<BOARD_COLS;j++)
+			board[i][j]=Blank;
 }
 
 /**
@@ -42,7 +60,27 @@ void Piezas::reset()
 **/ 
 Piece Piezas::dropPiece(int column)
 {
-    return Blank;
+int i=0;
+	if(column >=BOARD_COLS)
+		return Invalid;
+
+	for(i =BOARD_ROWS-1;i>=0;i--)
+	{
+		if(board[i][column] ==Blank)
+		{
+			board[i][column]=turn;
+			if(turn == X)
+				turn = O;
+			else
+				turn = X;
+			return board[i][column];	
+		}	
+	}
+	if (i<0)
+		return Invalid;
+	else 
+		return Blank;
+//    return Blank;
 }
 
 /**
@@ -51,7 +89,10 @@ Piece Piezas::dropPiece(int column)
 **/
 Piece Piezas::pieceAt(int row, int column)
 {
-    return Blank;
+	if(row >=BOARD_ROWS || column>=BOARD_COLS)
+		return Invalid;
+	return board[row][column];
+  //  return Blank;
 }
 
 /**
@@ -65,5 +106,112 @@ Piece Piezas::pieceAt(int row, int column)
 **/
 Piece Piezas::gameState()
 {
-    return Blank;
+int num_x_hor=0;
+int num_y_hor=0;
+int num_y_ver=0;
+int num_x_ver=0;
+int temp1 =0;
+int temp2 =0;
+int temp3 =0;
+int temp4 =0;
+int max_X=0;
+int max_O=0;
+	for(int i =0; i<BOARD_ROWS;i++)
+	{
+		for(int j =0 ; j<BOARD_COLS; j++)
+		{
+			if(board[i][j] ==Blank)
+			{	
+				return Invalid;
+			}	
+			if(board[i][j] ==X)
+			{
+				if(j==0 && i ==0)
+				{	
+					num_x_hor++;
+					if(board[i][j] == board[i+1][j])
+						{
+							if(num_x_ver <=2)
+								num_x_ver =2;
+							if(board[i][j] == board[i+2][j] && num_x_ver != BOARD_ROWS) 
+								num_x_ver++;	
+						}
+				}
+				if(j>0 && i==0 && board[i][j-1] == X)
+				{	
+					num_x_hor++;
+					if(board[i][j] == board[i+1][j])
+						{
+							if(num_x_ver <=2)
+								num_x_ver =2;
+							if(board[i][j] == board[i+2][j] && num_x_ver != BOARD_ROWS) 
+								num_x_ver++;	
+						}
+				}
+				if(j==0 && i ==1)
+					temp1++;
+				if(j>0 && i==1 &&temp1 ==0)
+					temp1++;
+				if(j>0 && i==1 && board[i][j-1] == X)
+					temp1++;
+				if(j==0 && i ==2)
+					temp2++;
+				if(j>0 && i==2 &&temp2 ==0)
+					temp2++;
+				if(j>0 && i==2 && board[i][j-1] == X)
+					temp2++;
+			}
+			else
+			{
+				if(j==0 && i ==0)
+				{	
+					num_y_hor++;
+					if(board[i][j] == board[i+1][j])
+						{
+							if(num_y_ver <=2)
+								num_y_ver =2;
+							if(board[i][j] == board[i+2][j] && num_y_ver != BOARD_ROWS) 
+								num_y_ver++;	
+						}
+				}
+				if(j>0 && i==0 && board[i][j-1] == O)
+				{	
+					num_y_hor++;
+					if(board[i][j] == board[i+1][j])
+						{
+							if(num_y_ver <=2)
+								num_y_ver =2;
+							if(board[i][j] == board[i+2][j] && num_y_ver != BOARD_ROWS) 
+								num_y_ver++;	
+						}
+				}
+				if(j==0 && i ==1)
+					temp3++;
+				if(j>0 && i==1 &&temp3 ==0)
+					temp3++;
+				if(j>0 && i==1 && board[i][j-1] == O)
+					temp3++;
+				if(j==0 && i ==2)
+					temp4++;
+				if(j>0 && i==2 &&temp4 ==0)
+					temp4++;
+				if(j>0 && i==2 && board[i][j-1] == O)
+					temp4++;
+			
+			}
+		}
+	}
+
+	num_x_hor = num_x_hor > (temp1 >temp2 ? temp1 : temp2) ? num_x_hor : (temp1>temp2 ? temp1 : temp2);	
+	max_X = num_x_hor > num_x_ver ? num_x_hor : num_x_ver;	
+	num_y_hor = num_y_hor > (temp3 >temp4 ? temp3 : temp4) ? num_y_hor : (temp3>temp4 ? temp3 : temp4);	
+	max_O = num_y_hor > num_y_ver ? num_y_hor : num_y_ver;	
+	
+	if(max_X > max_O)
+		return X;
+	if(max_O > max_X)
+		return O;
+	else
+		return Blank;
+   // return Blank;
 }
--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -14,7 +14,182 @@ class PiezasTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
+
 TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+TEST(PiezasTest, dropPiece_1x)
+{
+	char x;
+	Piezas test;
+	x= test.dropPiece(BOARD_COLS-2);
+	ASSERT_NE(x, Blank );
+}
+TEST(PiezasTest, dropPiece_2x)
+{
+	char x;
+	Piezas test;
+	x= test.dropPiece(BOARD_COLS-2);
+	x= test.dropPiece(BOARD_COLS-2);
+	ASSERT_NE(x, Blank );
+}
+TEST(PiezasTest, dropPiece_3x)
+{
+	char x;
+	Piezas test;
+	x= test.dropPiece(BOARD_COLS-2);
+	x= test.dropPiece(BOARD_COLS-2);
+	x= test.dropPiece(BOARD_COLS-2);
+	ASSERT_NE(x, Blank );
+}
+TEST(PiezasTest, dropPiece_4x)
+{
+	char x;
+	Piezas test;
+	x= test.dropPiece(BOARD_COLS-2);
+	x= test.dropPiece(BOARD_COLS-2);
+	x= test.dropPiece(BOARD_COLS-2);
+	x= test.dropPiece(BOARD_COLS-2);
+	ASSERT_EQ(x, Invalid );
+}
+TEST(PiezasTest, invalid_COl)
+{
+	char x;
+	Piezas test;
+	x= test.dropPiece(BOARD_COLS);
+	ASSERT_EQ(x, Invalid );
+}
+TEST(PiezasTest, changePiece_1x)
+{
+	char x;
+	Piezas test;
+	x= test.dropPiece(BOARD_COLS-2);
+	x= test.dropPiece(BOARD_COLS-2);
+	ASSERT_NE(x, X );
+}
+TEST(PiezasTest, changePiece_2x)
+{
+	char x;
+	Piezas test;
+	x= test.dropPiece(BOARD_COLS-2);
+	x= test.dropPiece(BOARD_COLS-2);
+	x= test.dropPiece(BOARD_COLS-2);
+	ASSERT_NE(x, O );
+}
+TEST(PiezasTest, fillBoard)
+{
+	char x;
+	Piezas test;
+	for(int i=0; i<BOARD_COLS;i++)
+		for(int j=0;j<BOARD_ROWS;j++)
+			x=test.dropPiece(i);
+	ASSERT_EQ(x, O );
+}
+TEST(PiezasTest, checkBlank)
+{
+	char x;
+	Piezas test;
+	x = test.pieceAt(1,1);
+	ASSERT_EQ(x, Blank );
+}
+TEST(PiezasTest, spotfilled_X)
+{
+	char x;
+	Piezas test;
+	for(int i=0; i<BOARD_COLS;i++)
+		for(int j=0;j<BOARD_ROWS;j++)
+			x=test.dropPiece(i);
+	x = test.pieceAt(1,1);
+	ASSERT_EQ(x, X );
+}
+TEST(PiezasTest, spotfilled_O)
+{
+	char x;
+	Piezas test;
+	for(int i=0; i<BOARD_COLS;i++)
+		for(int j=0;j<BOARD_ROWS;j++)
+			x=test.dropPiece(i);
+	x = test.pieceAt(1,0);
+	ASSERT_EQ(x, O );
+}
+TEST(PiezasTest, check_Xwin)
+{
+	char x;
+	Piezas test;
+	 test.dropPiece(0);
+	 test.dropPiece(0);
+	 test.dropPiece(1);
+	 test.dropPiece(0);
+	 test.dropPiece(1);
+	 test.dropPiece(1);
+	 test.dropPiece(2);
+	 test.dropPiece(2);
+	 test.dropPiece(2);
+	 test.dropPiece(3);
+	 test.dropPiece(3);
+	 test.dropPiece(3);
+	x = test.gameState();
+	ASSERT_EQ(x,X );
+}
+TEST(PiezasTest, check_Owin)
+{
+	char x;
+	Piezas test;
+	 test.dropPiece(0);
+	 test.dropPiece(1);
+	 test.dropPiece(1);
+	 test.dropPiece(2);
+	 test.dropPiece(1);
+	 test.dropPiece(3);
+	 test.dropPiece(0);
+	 test.dropPiece(2);
+	 test.dropPiece(2);
+	 test.dropPiece(0);
+	 test.dropPiece(3);
+	 test.dropPiece(3);
+	x = test.gameState();
+	ASSERT_EQ(x,O );
+}
+TEST(PiezasTest, testReset)
+{
+	char x;
+	Piezas test;
+	for(int i=0; i<BOARD_COLS;i++)
+		for(int j=0;j<BOARD_ROWS;j++)
+			test.dropPiece(i);
+	test.reset();
+	x = test.pieceAt(1,0);
+	ASSERT_EQ(x, Blank );
+}
+TEST(PiezasTest, test_turnreset)
+{
+	char x;
+	Piezas test;
+	for(int i=0; i<BOARD_COLS;i++)
+		for(int j=0;j<BOARD_ROWS;j++)
+			x=test.dropPiece(i);
+	test.reset();
+	x=test.dropPiece(0);
+	x=test.dropPiece(0);
+	x = test.pieceAt(1,0);
+	ASSERT_EQ(x, O );
+}
+TEST(PiezasTest, check_Nowin)
+{
+	char x;
+	Piezas test;
+	x = test.gameState();
+	ASSERT_EQ(x, Invalid );
+}
+TEST(PiezasTest, check_tie)
+{
+	char x;
+	Piezas test;
+	for(int i=0; i<BOARD_COLS;i++)
+		for(int j=0;j<BOARD_ROWS;j++)
+			x=test.dropPiece(i);
+	x = test.gameState();
+	ASSERT_EQ(x, Blank );
 }
